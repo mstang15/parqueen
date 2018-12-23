e1d5cb4a9e7594c294e44e1eb5b9cac7d129579b
Harper Bellows <harper.bellows@gmail.com>
2018-12-23 11:56:54 -0700

    add parkings create endpoint

---
+++ app/controllers/api/v1/parking_controller.rb
@@ -0,0 +1,16 @@
+class ParkingController < ApplicationController
+  before_action :curb_details
+
+  def show
+    render json: CurbSerializer.new(curb_details)
+  end
+
+  def create
+    parmas[:lat], params[:lon]
+  end
+
+  private
+  def curb_details
+    @curb_details ||= Parking.new(params[:lat], params[:lon])
+  end
+end
