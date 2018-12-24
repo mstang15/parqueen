fa2b4dfc30be96a847ded6c15bf797d49a2b6663
Harper Bellows <harper.bellows@gmail.com>
2018-12-24 14:04:02 -0700

    add fetch call to internal endpoint to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,38 @@
 //= require rails-ujs
 //= require activestorage
 // = require_tree .
+
+function getCurbLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showCurbPosition);
+  }
+  else {
+    var loc = "0,0";
+    window.location.href=`/parking?location=${loc}`;
+  }
+}
+
+function showCurbPosition(position) {
+  var lat = position['coords']['latitude'];
+  var lon = position['coords']['longitude'];
+  var url = "/api/v1/parking?" + "latitude="+ lat + "&longitude=" + lon;
+  fetch(url).then(response => response.json()).then((response) => {this.printCurbDetails(response)});
+}
+
+function printCurbDetails(response) {
+  if (response['no_curbs_reason']) {
+    document.getElementById("spot_info").innerHTML = (response['no_curbs_reason'])
+  }
+  else if (response['features'][0]['properties']['uses']['use'] === 'park') {
+    document.getElementById("spot_info").innerHTML = "You parked on the "
+    + (response['features'][0]['properties']['metadata']['side_of_street'])
+    + " side of "
+    + (response['features'][0]['properties']['metadata']['street_name'])
+    + " between "
+    + (response['features'][0]['properties']['metadata']['start_street_name'])
+    + " and "
+    + (response['features'][0]['properties']['metadata']['end_street_name'])}
+  else if (response){
+    document.getElementById("spot_info").innerHTML = 'YOU CANNOT LEGALLY PARK HERE!';
+  }
+}
