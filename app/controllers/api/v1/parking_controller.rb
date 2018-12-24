bd4a5335fd62db5fbb3e1cc88e119162590cb6c0
Harper Bellows <harper.bellows@gmail.com>
2018-12-24 15:46:57 -0700

    refactor curb_details method to be a private method

--- app/controllers/api/v1/parking_controller.rb
+++ app/controllers/api/v1/parking_controller.rb
@@ -2,16 +2,11 @@ class Api::V1::ParkingController < ApiController
   before_action :curb_details
 
   def show
-    render json: CurbSerializer.new(curb_details)
-    binding.pry
-  end
-
-  def create
-
+    render json: CurbSerializer.new(curb_details).serialized_json
   end
 
   private
   def curb_details
-    @curb_details ||= CurbSearch.new(params[:latitude], params[:longitude])
+    @curb_details = CurbSearch.new(params[:latitude], params[:longitude])
   end
 end
