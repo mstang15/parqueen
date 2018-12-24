ccd2397558207d1b1a93d5be6da3782dc7e8c45b
Harper Bellows <harper.bellows@gmail.com>
2018-12-24 16:52:02 -0700

    add passing edge case to  request spec to test that unrecoginzable coordinates returns appropriate message

--- spec/requests/api/v1/curb_details_request_spec.rb
+++ spec/requests/api/v1/curb_details_request_spec.rb
@@ -23,5 +23,27 @@ describe 'GET request' do
         expect(returned_curb_details[:data][:attributes][:message]).to eq(message_result)
       end
     end
+
+    it 'does not show curb details for unrecognized coordinates' do
+      VCR.use_cassette('no_curb_data') do
+        lat = 39.6729249
+        lon = -104.8777542
+        get api_v1_parking_path(latitude: lat, longitude: lon)
+        # get "/api/v1/parking?latitude=#{lat}&longitude=#{lon}"
+
+        expect(response.status).to eq(200)
+
+        returned_curb_details = JSON.parse(response.body, symbolize_names: true)
+
+        message_result = "The location you supplied is not in a city we have curb data for. To see the list of cities we support, visit https://coord.co/locations"
+        
+        expect(returned_curb_details).to be_a(Hash)
+        expect(returned_curb_details).to have_key(:data)
+        expect(returned_curb_details[:data]).to have_key(:attributes)
+        expect(returned_curb_details[:data][:attributes]).to have_key(:parking)
+        expect(returned_curb_details[:data][:attributes]).to have_key(:message)
+        expect(returned_curb_details[:data][:attributes][:message]).to eq(message_result)
+      end
+    end
   end
 end
