c80b1222e4810d355059ff1ce2f2f324040894b4
Harper Bellows <harper.bellows@gmail.com>
2018-12-24 16:47:47 -0700

    refactor curb_information poro to handle error messages in coord json

--- app/models/curb_information.rb
+++ app/models/curb_information.rb
@@ -1,19 +1,29 @@
 class CurbInformation
-  attr_reader            :street_name,
-                         :start_cross_street,
-                         :end_cross_street,
-                         :side_of_street,
-                         :legal
+  attr_reader :no_curbs,
+              :street_name,
+              :start_cross_street,
+              :end_cross_street,
+              :side_of_street,
+              :legal
 
   def initialize(data)
-  @street_name        = data[:properties][:metadata][:street_name]
-  @start_cross_street = data[:properties][:metadata][:start_street_name]
-  @end_cross_street   = data[:properties][:metadata][:end_street_name]
-  @side_of_street     = data[:properties][:metadata][:side_of_street]
-    if data[:properties][:uses][:use] == "park"
-      @legal          = true
+    set_curb_attributes(data)
+  end
+
+  def set_curb_attributes(data)
+    if data[:features].empty?
+      @no_curbs = data[:no_curbs_reason]
     else
-      @legal          = false
+      features = data[:features][0]
+      @street_name = features[:properties][:metadata][:street_name]
+      @start_cross_street = features[:properties][:metadata][:start_street_name]
+      @end_cross_street = features[:properties][:metadata][:end_street_name]
+      @side_of_street = features[:properties][:metadata][:side_of_street]
+      if features[:properties][:uses][:use] == "park"
+        @legal = true
+      else
+        @legal = false
+      end
     end
   end
 end
