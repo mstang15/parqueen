589d295c21ac1486de0f4542d49bee9d5e646cf3
Harper Bellows <harper.bellows@gmail.com>
2018-12-24 10:35:40 -0700

    add namespaced api routes for parking show and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   get '/home', to: 'home#show'
   get '/parking', to: "parking#show", as: 'parking'
   get '/logout', to: 'sessions#destroy'
+
+  namespace :api do
+    namespace :v1 do
+      get 'parking', to: 'parking#show'
+      post '/parking', to: 'parking#create'
+    end
+  end
 end
