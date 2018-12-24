a99ec08b22050ef928f79ebdb5e3cebf04985f70
Harper Bellows <harper.bellows@gmail.com>
2018-12-24 15:49:05 -0700

    add passing request spec for parking#show endpoint - returned json object includes message

--- spec/requests/api/v1/curb_details_request_spec.rb
+++ spec/requests/api/v1/curb_details_request_spec.rb
@@ -3,17 +3,25 @@ require 'rails_helper'
 describe 'GET request' do
   describe 'user visits /api/v1/parking' do
     it 'shows curb details' do
-      lat = 37.802756844713855
-      lon = -122.40579354398695
-      visit "/api/v1/parking?latitude=#{lat}&longitude=#{lon}"
+      VCR.use_cassette('get_curb_data') do
+        lat = 37.802756844713855
+        lon = -122.40579354398695
+        get api_v1_parking_path(latitude: lat, longitude: lon)
+        # get "/api/v1/parking?latitude=#{lat}&longitude=#{lon}"
 
-      # expect(response.status).to eq(200)
+        expect(response.status).to eq(200)
 
-      curb_details = JSON.parse(response.body, symbolize_names: true)
+        returned_curb_details = JSON.parse(response.body, symbolize_names: true)
 
-      expect(curb_details).to be_a(Hash)
-      expect(curb_details).to have_key(:data)
-      expect(curb_details[:data]).to have_key(:message)
+        message_result = "You are parked on the NE side of Telegraph Hill Boulevard between DEAD END and Lombard Street."
+        
+        expect(returned_curb_details).to be_a(Hash)
+        expect(returned_curb_details).to have_key(:data)
+        expect(returned_curb_details[:data]).to have_key(:attributes)
+        expect(returned_curb_details[:data][:attributes]).to have_key(:parking)
+        expect(returned_curb_details[:data][:attributes]).to have_key(:message)
+        expect(returned_curb_details[:data][:attributes][:message]).to eq(message_result)
+      end
     end
   end
 end
