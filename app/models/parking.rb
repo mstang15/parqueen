9093b429d378dd12a4e36859422c2b3e26ea81a8
Harper Bellows <harper.bellows@gmail.com>
2018-12-26 15:59:38 -0700

    finalize parking model to save curb data and user_id

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,17 +1,17 @@
 class Parking < ApplicationRecord
+  belongs_to :user
 
-  # def self.create_from_json(json_data)
-  #   Parking.create(
-  #     curb_id: json_data[:curb_id]
-  #     latitude: json_data[:latitude]
-  #     longitude: json_data[:longitude]
-  #     legal: json_data[:legal]
-  #     street_name: json_data[:street_name]
-  #     start_cross_street: json_data[:start_cross_street]
-  #     end_cross_street: json_data[:end_cross_street]
-  #     side_of_street: json_data[:side_of_street]
-
-  #   )
-  # end
+  def self.create_from_json(json_data)
+    Parking.create(
+      curb_id: json_data[:curb_id],
+      latitude: json_data[:latitude],
+      longitude: json_data[:longitude],
+      legal: json_data[:legal],
+      street_name: json_data[:street_name],
+      start_cross_street: json_data[:start_cross_street],
+      end_cross_street: json_data[:end_cross_street],
+      side_of_street: json_data[:side_of_street],
+      user_id: Base64.decode64(json_data[:user_id])
+    )
+  end
 end
-
