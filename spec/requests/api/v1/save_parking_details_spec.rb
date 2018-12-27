7fc331e8b06c94bcca1f8f22582a2f7e91c34f75
Harper Bellows <harper.bellows@gmail.com>
2018-12-27 09:25:48 -0700

    add passing post request spec to save user location

---
+++ spec/requests/api/v1/save_parking_details_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe 'POST request' do
+  describe 'user saves curb details to /api/v1/parking' do
+    it 'saves curb details to database and renders a success message' do
+      curb_payload = {
+          "data": {
+              "id": "Curb Details",
+              "type": "curb",
+              "attributes": {
+                  "parking": {
+                      "user_id": "MQ==",
+                      "curb_id": "c2Y6Mzk3OQ",
+                      "latitude": 37.80221017610328,
+                      "longitude": -122.4053808884732,
+                      "street_name": "Telegraph Hill Boulevard",
+                      "start_cross_street": "Lombard Street",
+                      "end_cross_street": "DEAD END",
+                      "side_of_street": "SW",
+                      "legal": true
+                  },
+                  "message": "You are parked on the SW side of Telegraph Hill Boulevard between Lombard Street and DEAD END."
+              }
+          }
+      }
+
+      post '/api/v1/parking', params: curb_payload
+
+      expect(response.status).to eq(200)
+
+      returned_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(returned_json[:message]).to eq("This location has been saved.")
+    end
+
+    it 'does not save duplicate curb details to database and renders an already saved message' do
+    curb_1 = Parking.create(user_id: 1, curb_id: "c2Y6Mzk3OQ", latitude: 37.80221017610328, longitude: -122.4053808884732, 
+                            street_name: "Telegraph Hill Boulevard", start_cross_street: "Lombard Street", end_cross_street: "DEAD END",
+                            side_of_street: "SW", legal: true)
+      curb_payload = {
+          "data": {
+              "id": "Curb Details",
+              "type": "curb",
+              "attributes": {
+                  "parking": {
+                      "user_id": "MQ==",
+                      "curb_id": "c2Y6Mzk3OQ",
+                      "latitude": 37.80221017610328,
+                      "longitude": -122.4053808884732,
+                      "street_name": "Telegraph Hill Boulevard",
+                      "start_cross_street": "Lombard Street",
+                      "end_cross_street": "DEAD END",
+                      "side_of_street": "SW",
+                      "legal": true
+                  },
+                  "message": "You are parked on the SW side of Telegraph Hill Boulevard between Lombard Street and DEAD END."
+              }
+          }
+      }
+
+      post '/api/v1/parking', params: curb_payload
+
+      expect(response.status).to eq(200)
+
+      returned_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(returned_json[:message]).to eq("This location has been saved.")
+    end
+  end
+end
