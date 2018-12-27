1a1f3d780555af77ee609376041d585a50d0de01
Harper Bellows <harper.bellows@gmail.com>
2018-12-27 10:57:38 -0700

    add responsive styling and toggle-able button for cleaner view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,46 @@
 //= require activestorage
 // = require_tree .
 
+$(document).ready(function() {
+  $('#park-here-btn, #save-btn').click(function() {
+    $('#park-here-btn').toggle()
+    $('#save-btn').toggle()
+  })
+})
+
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('api-map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 20
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
 function getCurbLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showCurbPosition);
