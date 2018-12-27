4b98324cbaf79c3dfee37304b63d158134f92c8e
Harper Bellows <harper.bellows@gmail.com>
2018-12-27 09:39:09 -0700

    add passing post request spec for existing entry in database

--- spec/requests/api/v1/save_parking_details_spec.rb
+++ spec/requests/api/v1/save_parking_details_spec.rb
@@ -34,7 +34,8 @@ describe 'POST request' do
     end
 
     it 'does not save duplicate curb details to database and renders an already saved message' do
-    curb_1 = Parking.create(user_id: 1, curb_id: "c2Y6Mzk3OQ", latitude: 37.80221017610328, longitude: -122.4053808884732, 
+    user = User.create(uid: '123', name: 'Grace Hopper', email: 'sweet@sweetness.com', google_token: 'abc123')
+    curb_1 = Parking.create(user_id: user.id, curb_id: "c2Y6Mzk3OQ", latitude: 37.80221017610328, longitude: -122.4053808884732, 
                             street_name: "Telegraph Hill Boulevard", start_cross_street: "Lombard Street", end_cross_street: "DEAD END",
                             side_of_street: "SW", legal: true)
       curb_payload = {
@@ -60,11 +61,11 @@ describe 'POST request' do
 
       post '/api/v1/parking', params: curb_payload
 
-      expect(response.status).to eq(200)
+      expect(response.status).to eq(400)
 
       returned_json = JSON.parse(response.body, symbolize_names: true)
 
-      expect(returned_json[:message]).to eq("This location has been saved.")
+      expect(returned_json[:message]).to eq("You have already saved this location.")
     end
   end
 end
